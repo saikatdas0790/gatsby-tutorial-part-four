1b696480f6e2b7ff0c60e377cc00f2f9efaaaa7c
Saikat Das <saikatdas0790@gmail.com>
2019-01-21 14:42:50 +0530

    Add manifest, service workers and React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,20 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/images/icon.png"
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`
   ]
 };
